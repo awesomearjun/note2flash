b6d27e7b495f3a97df6c25c9ca8273341c8fddc3
Arj404 <64511698+awesomearjun@users.noreply.github.com>
2025-01-25 05:17:17 +0000

    setup basic typescript project

---
+++ note2flash/src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let x = document.querySelector("h1");
+x.textContent = "bruh";
---
+++ note2flash/src/index.ts
@@ -0,0 +1,2 @@
+let x: HTMLHeadingElement = document.querySelector("h1") as HTMLHeadingElement;
+x.textContent = "bruh";
